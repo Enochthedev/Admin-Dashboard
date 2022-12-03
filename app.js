dc0bc365b4f31772c323b2c1a4b7823882a8d7f6
Enochthedev <enochjesse884@gmail.com>
2022-12-03 10:20:22 +0100

    reverted to 2

--- app.js
+++ app.js
@@ -2,17 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require('cors');
-const path = require('path');
 //get index.html
 
-app.use(express.static('public'));
-console.log(__dirname);
-app.use('/css', express.static(path.join(__dirname, 'public/css')));
-app.use('/js', express.static(path.join(__dirname, 'public/js')));
-app.use('/img', express.static(path.join(__dirname, 'public/img')));
-
-//set view engine
-app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -25,5 +16,5 @@ app.listen(port, () => {
 
 // Path: routes/index.js
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    res.send('Hello World!')
     })
